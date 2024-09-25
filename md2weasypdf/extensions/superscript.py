f0e5393d0a0bef0f44768f6e42e6156543dc1685
Manuel Stingl <manuel@stingl.st>
2024-09-25 15:25:22 +0200

    fix and improve mermaid generation, fix superscript

--- md2weasypdf/extensions/superscript.py
+++ md2weasypdf/extensions/superscript.py
@@ -4,4 +4,4 @@ from markdown.inlinepatterns import SimpleTagInlineProcessor
 
 class SuperscriptExtension(Extension):
     def extendMarkdown(self, md):
-        md.inlinePatterns.register(SimpleTagInlineProcessor(r"(\~)([^\^]+)\^", "sup"), "superscript", 150)
+        md.inlinePatterns.register(SimpleTagInlineProcessor(r"(\^)([^\^]+)\^", "sup"), "superscript", 150)
